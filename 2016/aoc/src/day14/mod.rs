45fe8943664f71b2a2a7dfe68ac9e7581313b463
Matthew Walton <matthew@matthew-walton.co.uk>
2016-12-18 13:19:48 +0000

    Day 14 takes ages to run!

---
+++ 2016/aoc/src/day14/mod.rs
@@ -0,0 +1,184 @@
+use md5;
+use rustc_serialize::hex::ToHex;
+use std::string::ToString;
+use std::collections::HashMap;
+
+pub fn do_day14() {
+    let mut space = SearchSpace::new("jlmsuwbz", false);
+    let indexes = space.get_valid_keys(64);
+
+    println!("Index of 64th key is {}", indexes[63]);
+
+    let mut stretched_space = SearchSpace::new("jlmsuwbz", true);
+    let stretched_indexes = stretched_space.get_valid_keys(64);
+
+    println!("Index of 64th stretched key is {}", stretched_indexes[63]);
+}
+
+fn md5sum(input: &str, stretched: bool) -> String {
+    let base = md5::compute(input.as_bytes()).to_hex();
+    if stretched {
+        stretch_hash(&base, 2016)
+    } else {
+        base
+    }
+}
+
+fn stretch_hash(input: &str, count: usize) -> String {
+    if count == 1 {
+        md5::compute(input.as_bytes()).to_hex()
+    } else {
+        md5::compute(stretch_hash(input, count - 1).as_bytes()).to_hex()
+    }
+}
+
+fn salted_hash(index: u64, salt: &String, stretched: bool) -> String {
+    md5sum(&(salt.clone() + &index.to_string()), stretched)
+}
+
+/// Does this byte slice contain a triple-repeated character
+fn contains_triple(haystack: &[u8]) -> Option<char> {
+    if haystack.len() < 3 {
+        return None;
+    }
+    for i in 0..haystack.len() - 2 {
+        let thischar = haystack[i];
+        if haystack[i + 1] == thischar && haystack[i + 2] == thischar {
+            return Some(thischar as char);
+        }
+    }
+
+    None
+}
+
+fn contains_pentuple_of(haystack: &[u8], c: char) -> bool {
+    if haystack.len() < 5 {
+        return false;
+    }
+    let cc = c as u8;
+    for i in 0..haystack.len() - 4 {
+        if haystack[i] == cc && haystack[i + 1] == cc && haystack[i + 2] == cc &&
+           haystack[i + 3] == cc && haystack[i + 4] == cc {
+            return true;
+        }
+    }
+
+    false
+}
+
+struct SearchSpace {
+    generated: HashMap<u64, String>,
+    salt: String,
+    stretch: bool,
+}
+
+impl SearchSpace {
+    fn new(salt: &str, stretch: bool) -> SearchSpace {
+        SearchSpace {
+            generated: HashMap::new(),
+            salt: salt.to_owned(),
+            stretch: stretch,
+        }
+    }
+
+    fn generate(&mut self, index: u64) -> String {
+        if let Some(existing) = self.generated.get(&index) {
+            return existing.clone();
+        }
+
+        let new = salted_hash(index, &self.salt, self.stretch);
+        self.generated.insert(index, new.clone());
+        new
+    }
+
+    fn first_triple_from(&mut self, start: u64) -> (u64, char) {
+        let hash = self.generate(start);
+        if let Some(c) = contains_triple(hash.as_bytes()) {
+            (start, c)
+        } else {
+            self.first_triple_from(start + 1)
+        }
+    }
+
+    fn first_pentuple_of_from_to(&mut self, of: char, start: u64, limit: u64) -> Option<u64> {
+        let hash = self.generate(start);
+        if contains_pentuple_of(hash.as_bytes(), of) {
+            Some(start)
+        } else if start + 1 == limit {
+            None
+        } else {
+            self.first_pentuple_of_from_to(of, start + 1, limit)
+        }
+    }
+
+    fn find_first_key_from(&mut self, start: u64) -> u64 {
+        let (idx, c) = self.first_triple_from(start);
+        if let Some(_) = self.first_pentuple_of_from_to(c, idx + 1, idx + 1001) {
+            idx
+        } else {
+            self.find_first_key_from(idx + 1)
+        }
+    }
+
+    /// return the indexes of valid keys found, up to count keys
+    fn get_valid_keys(&mut self, count: u64) -> Vec<u64> {
+        let mut v = Vec::new();
+        let mut current_index = 0;
+
+        loop {
+            if v.len() as u64 == count {
+                return v;
+            }
+
+            let key_index = self.find_first_key_from(current_index);
+            v.push(key_index);
+            current_index = key_index + 1;
+        }
+    }
+}
+
+#[test]
+fn test_example_stuff() {
+    let salt = "abc".to_owned();
+    // we know index 18 will produce a hash containing "cc38887a5"
+    assert!(salted_hash(18, &salt, false).find("cc38887a5").is_some());
+
+    let mut space = SearchSpace::new("abc");
+    // we also know that's the first triple
+    let (first_index, first_char) = space.first_triple_from(0);
+    assert_eq!(first_index, 18);
+    assert_eq!(first_char, '8');
+
+    // and that the next 1000 don't contain five 8
+    assert!(space.first_pentuple_of_from_to('8', 19, 1019).is_none());
+
+    // the next triple is 39
+    let (second_index, second_char) = space.first_triple_from(19);
+    assert_eq!(second_index, 39);
+    assert_eq!(second_char, 'e');
+
+    // and we know 816 has a pentuple e
+    let first_pentuple_idx = space.first_pentuple_of_from_to('e', 40, 1040);
+    assert_eq!(first_pentuple_idx, Some(816));
+
+    assert_eq!(space.find_first_key_from(0), 39);
+    assert_eq!(space.find_first_key_from(40), 92);
+
+    let indexes = space.get_valid_keys(2);
+    assert_eq!(indexes[0], 39);
+    assert_eq!(indexes[1], 92);
+}
+
+#[test]
+fn test_contains_triple() {
+    assert_eq!(contains_triple(b"555"), Some('5'));
+    assert_eq!(contains_triple(b"556"), None);
+    assert_eq!(contains_triple(b"fried eggs are greeeeeat"), Some('e'));
+}
+
+#[test]
+fn test_contains_pentuple_of() {
+    assert_eq!(contains_pentuple_of(b"555", '5'), false);
+    assert_eq!(contains_pentuple_of(b"556", '5'), false);
+    assert_eq!(contains_pentuple_of(b"fried eggs are greeeeeat", 'e'), true);
+}
--- 2016/aoc/src/main.rs
+++ 2016/aoc/src/main.rs
@@ -10,21 +10,22 @@ extern crate nom;
 #[macro_use]
 extern crate nom_test_helpers;
 
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
-mod day10;
-// mod day11;
-mod day12;
-mod day13;
+// mod day1;
+// mod day2;
+// mod day3;
+// mod day4;
+// mod day5;
+// mod day6;
+// mod day7;
+// mod day8;
+// mod day9;
+// mod day10;
+// // mod day11;
+// mod day12;
+// mod day13;
+mod day14;
 
-mod nom_helpers;
+// mod nom_helpers;
 
 use std::env;
 
@@ -36,76 +37,80 @@ fn main() {
         .expect("Unable to parse the provided argument - is it a number?");
 
     match desired_daynum {
-        1 => {
-            print_day_header(1, false);
-            day1::do_day1();
-        }
-        2 => {
-            print_day_header(2, false);
-            let input = include_str!("inputs/day2_input.txt");
-            day2::do_day2(input);
-        }
-        3 => {
-            print_day_header(3, false);
-
-            day3::do_day3();
-        }
-        4 => {
-            print_day_header(4, false);
-
-            day4::do_day4();
-        }
-        5 => {
-            print_day_header(5, true);
-
-            day5::do_day5();
-        }
-        6 => {
-            print_day_header(6, false);
-            let input = include_str!("inputs/day6_input.txt");
-
-            day6::do_day6(input);
-        }
-        7 => {
-            print_day_header(7, false);
-            let input = include_str!("inputs/day7_input.txt");
-
-            day7::do_day7(input);
-        }
-        8 => {
-            print_day_header(8, false);
-            let input = include_str!("inputs/day8_input.txt");
-
-            day8::do_day8(input);
-        }
-        9 => {
-            print_day_header(9, false);
-            let input = include_str!("inputs/day9_input.txt");
-
-            day9::do_day9(input);
-        }
-        10 => {
-            print_day_header(10, false);
-            let input = include_str!("inputs/day10_input.txt");
-
-            day10::do_day10(input);
-        }
-        11 => {
-            print_day_header(11, true);
-            // let input = include_str!("inputs/day11_input.txt");
-
-            // day11::do_day11(input);
-            println!("Day 11 incomplete");
-        }
-        12 => {
-            print_day_header(12, false);
-            let input = include_str!("inputs/day12_input.txt");
-
-            day12::do_day12(input);
-        }
-        13 => {
-            print_day_header(13, false);
-            day13::do_day13();
+        // 1 => {
+        //     print_day_header(1, false);
+        //     day1::do_day1();
+        // }
+        // 2 => {
+        //     print_day_header(2, false);
+        //     let input = include_str!("inputs/day2_input.txt");
+        //     day2::do_day2(input);
+        // }
+        // 3 => {
+        //     print_day_header(3, false);
+        //
+        //     day3::do_day3();
+        // }
+        // 4 => {
+        //     print_day_header(4, false);
+        //
+        //     day4::do_day4();
+        // }
+        // 5 => {
+        //     print_day_header(5, true);
+        //
+        //     day5::do_day5();
+        // }
+        // 6 => {
+        //     print_day_header(6, false);
+        //     let input = include_str!("inputs/day6_input.txt");
+        //
+        //     day6::do_day6(input);
+        // }
+        // 7 => {
+        //     print_day_header(7, false);
+        //     let input = include_str!("inputs/day7_input.txt");
+        //
+        //     day7::do_day7(input);
+        // }
+        // 8 => {
+        //     print_day_header(8, false);
+        //     let input = include_str!("inputs/day8_input.txt");
+        //
+        //     day8::do_day8(input);
+        // }
+        // 9 => {
+        //     print_day_header(9, false);
+        //     let input = include_str!("inputs/day9_input.txt");
+        //
+        //     day9::do_day9(input);
+        // }
+        // 10 => {
+        //     print_day_header(10, false);
+        //     let input = include_str!("inputs/day10_input.txt");
+        //
+        //     day10::do_day10(input);
+        // }
+        // 11 => {
+        //     print_day_header(11, true);
+        //     // let input = include_str!("inputs/day11_input.txt");
+        //
+        //     // day11::do_day11(input);
+        //     println!("Day 11 incomplete");
+        // }
+        // 12 => {
+        //     print_day_header(12, false);
+        //     let input = include_str!("inputs/day12_input.txt");
+        //
+        //     day12::do_day12(input);
+        // }
+        // 13 => {
+        //     print_day_header(13, false);
+        //     day13::do_day13();
+        // }
+        14 => {
+            print_day_header(14, true);
+            day14::do_day14();
         }
         _ => println!("I'm sorry, I can't handle day {} yet", desired_daynum),
     }
