5ff8f77c8995a5f4e4f27d33bd34b6ca8d731367
Matthew Walton <matthew@matthew-walton.co.uk>
2017-12-06 16:17:43 +0000

    2017 Day 6 Rather faster

--- aoc2017/src/day6/mod.rs
+++ aoc2017/src/day6/mod.rs
@@ -5,87 +5,40 @@ use util::timed_repeatedly;
 pub fn go(reps: usize) {
     let input = parse_input(include_str!("input.txt"));
 
-    let (result, time) = timed_repeatedly(reps, || part1(&input));
-    println!("[{}ms] {} steps", time, result);
-
-    let (result, time) = timed_repeatedly(reps, || part2(&input));
-    println!("[{}ms] {} steps in loop", time, result);
-}
-
-fn part1(input: &Vec<u32>) -> usize {
-    let mut buckets = input.clone();
-    redistribute_until_repeated(&mut buckets)
-}
-
-fn part2(input: &Vec<u32>) -> usize {
-    let mut buckets = input.clone();
-    find_redistribute_loop_size(&mut buckets)
+    let ((part1, part2), time) = timed_repeatedly(reps, || {
+        let mut buckets = input.clone();
+        explore_loop(&mut buckets)
+    });
+    println!("[{}ms] {} steps until loop, {} steps in loop",
+             time,
+             part1,
+             part2);
 }
 
 fn parse_input(input: &str) -> Vec<u32> {
     input.split_whitespace().filter_map(|x| u32::from_str(x).ok()).collect()
 }
 
-fn redistribute_until_repeated(buckets: &mut Vec<u32>) -> usize {
-    let mut steps = 0;
-    let mut states = HashSet::new();
-    states.insert(buckets.clone());
-
-    loop {
-        steps += 1;
-        let mut largest = 0;
-        let mut largest_index = 0;
-        for (i, &x) in buckets.iter().enumerate() {
-            if x > largest {
-                largest = x;
-                largest_index = i;
-            }
-        }
-        #[cfg(test)]
-        println!("{} steps, largest bucket is {} at index {}",
-                 steps,
-                 largest,
-                 largest_index);
-        redistribute(buckets, largest_index);
-
-        if states.contains(buckets) {
-            break;
-        }
-        states.insert(buckets.clone());
-    }
-
-    steps
-}
-
-fn find_redistribute_loop_size(buckets: &mut Vec<u32>) -> usize {
+fn explore_loop(buckets: &mut Vec<u32>) -> (usize, usize) {
     let mut loop_started = false;
-    let mut steps = 0;
+    let mut steps_until_repeat = 0;
+    let mut steps_in_loop = 0;
     let mut states = HashSet::new();
     states.insert(buckets.clone());
     let mut first_repeated_state = buckets.clone();
 
     loop {
-        let mut largest = 0;
-        let mut largest_index = 0;
-        for (i, &x) in buckets.iter().enumerate() {
-            if x > largest {
-                largest = x;
-                largest_index = i;
-            }
-        }
-        #[cfg(test)]
-        println!("{} steps, largest bucket is {} at index {}",
-                 steps,
-                 largest,
-                 largest_index);
+        let largest_index = largest_bucket_index(&buckets);
+
         redistribute(buckets, largest_index);
 
         if loop_started {
-            steps += 1;
+            steps_in_loop += 1;
             if *buckets == first_repeated_state {
                 break;
             }
         } else {
+            steps_until_repeat += 1;
             if states.contains(buckets) {
                 loop_started = true;
                 first_repeated_state = buckets.clone();
@@ -94,7 +47,7 @@ fn find_redistribute_loop_size(buckets: &mut Vec<u32>) -> usize {
         states.insert(buckets.clone());
     }
 
-    steps
+    (steps_until_repeat, steps_in_loop)
 }
 
 /// Conducts a single redistribute step
@@ -111,6 +64,19 @@ fn redistribute(buckets: &mut Vec<u32>, source: usize) {
         available -= 1;
         current_bucket = next_bucket(current_bucket, num_buckets);
     }
+}
+
+fn largest_bucket_index(buckets: &Vec<u32>) -> usize {
+    let mut largest = 0;
+    let mut largest_index = 0;
+    // find largest bucket
+    for (i, &x) in buckets.iter().enumerate() {
+        if x > largest {
+            largest = x;
+            largest_index = i;
+        }
+    }
+    largest_index
 }
 
 /// Given the current bucket, and the number of buckets, return the 0-index of the next bucket.
@@ -137,15 +103,9 @@ mod tests {
         #[test]
         fn full_run() {
             let mut buckets = vec![0, 2, 7, 0];
-            let count = redistribute_until_repeated(&mut buckets);
-            assert_eq!(count, 5);
-        }
-
-        #[test]
-        fn loop_size() {
-            let mut buckets = vec![0, 2, 7, 0];
-            let count = find_redistribute_loop_size(&mut buckets);
-            assert_eq!(count, 4);
+            let (until_loop, in_loop) = explore_loop(&mut buckets);
+            assert_eq!(until_loop, 5, "Steps until loop");
+            assert_eq!(in_loop, 4, "Steps in loop");
         }
     }
 }
