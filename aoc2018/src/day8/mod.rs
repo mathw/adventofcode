cd0c4e10d8518e76c2d8d92617e7720cca4d0338
Matthew Walton <matthew@mathw.me.uk>
2018-12-13 19:56:16 +0000

    2018: Day 8 Part 1

--- aoc2018/src/day8/mod.rs
+++ aoc2018/src/day8/mod.rs
@@ -1,26 +1,54 @@
 mod node;
 
+use self::node::Node;
 use crate::day::Day;
 use std::str::FromStr;
 use std::sync::mpsc::Sender;
 
 pub struct Day8 {
-    input: Vec<u8>,
+    input: &'static str,
 }
 
 impl Day8 {
     pub fn new() -> Day8 {
         Day8 {
-            input: include_str!("input.txt")
-                .split(' ')
-                .filter_map(|w| u8::from_str(w).ok())
-                .collect(),
+            input: include_str!("input.txt"),
         }
     }
 }
 
 impl Day for Day8 {
-    fn part1(&mut self, _sender: &Sender<String>) {}
+    fn part1(&mut self, _sender: &Sender<String>) {
+        match sum_metadata(self.input) {
+            Err(e) => _sender.send(format!("Error parsing nodes: {}", e)).unwrap(),
+            Ok(sum) => {
+                _sender
+                    .send(format!("The metadata sum is {}", sum))
+                    .unwrap();
+            }
+        }
+    }
 
     fn part2(&mut self, _sender: &Sender<String>) {}
+}
+
+fn sum_metadata(input: &str) -> Result<u16, String> {
+    let nums = input
+        .trim()
+        .split(' ')
+        .filter_map(|w| u8::from_str(w).ok())
+        .collect::<Vec<u8>>();
+    let node = Node::from_u8(&mut nums.into_iter())?;
+
+    Ok(node.sum_metadata())
+}
+
+#[test]
+fn example_part_one() {
+    let input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
+    ";
+
+    let result = sum_metadata(input);
+
+    assert_eq!(result, Ok(138));
 }
--- aoc2018/src/day8/node.rs
+++ aoc2018/src/day8/node.rs
@@ -1,12 +1,40 @@
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct Node {
-    child_nodes: Vec<Box<Node>>,
+    children: Vec<Node>,
     metadata: Vec<u8>,
 }
 
 impl Node {
-    pub fn from_u8<I: IntoIterator<Item = u8>>(numbers: I) -> Result<Node, String> {
-        Err("Not implemented".into())
+    pub fn from_u8<I: Iterator<Item = u8>>(numbers: &mut I) -> Result<Node, String> {
+        let num_children = numbers
+            .next()
+            .ok_or("Iterator did not yield number of child nodes".to_owned())?;
+        let num_metadata = numbers
+            .next()
+            .ok_or("Iterator did not yield count of metadata".to_owned())?;
+
+        let mut children = Vec::new();
+        for _ in 0..num_children {
+            children.push(Node::from_u8(numbers)?);
+        }
+
+        let metadata = numbers.take(num_metadata as usize).collect();
+
+        Ok(Node { children, metadata })
+    }
+
+    pub fn children(&self) -> &Vec<Node> {
+        &self.children
+    }
+
+    pub fn metadata(&self) -> &Vec<u8> {
+        &self.metadata
+    }
+
+    pub fn sum_metadata(&self) -> u16 {
+        let me = self.metadata.iter().fold(0, |acc, x| acc + *x as u16);
+        let children: u16 = self.children.iter().map(|c| c.sum_metadata()).sum();
+        me + children
     }
 }
 
@@ -14,13 +42,101 @@ impl Node {
 fn parse_empty_node() {
     let nums = vec![0, 0];
 
-    let node = Node::from_u8(nums);
+    let node = Node::from_u8(&mut nums.into_iter());
 
     assert_eq!(
         node,
         Ok(Node {
-            child_nodes: vec![],
+            children: vec![],
             metadata: vec![]
         })
     );
+}
+
+#[test]
+fn parse_node_with_no_children_and_metadata() {
+    let nums = vec![0, 3, 1, 2, 3];
+
+    let node = Node::from_u8(&mut nums.into_iter());
+
+    assert_eq!(
+        node,
+        Ok(Node {
+            children: vec![],
+            metadata: vec![1, 2, 3]
+        })
+    );
+}
+
+#[test]
+fn parse_node_with_no_children_and_metadata_and_leftover_numbers() {
+    let nums = vec![0, 3, 1, 2, 3, 5, 6, 45];
+    let mut iter = nums.into_iter();
+
+    let node = Node::from_u8(&mut iter);
+
+    assert_eq!(
+        node,
+        Ok(Node {
+            children: vec![],
+            metadata: vec![1, 2, 3]
+        })
+    );
+
+    assert_eq!(iter.next(), Some(5));
+}
+
+#[test]
+fn parse_node_with_one_child_and_two_metadata() {
+    let nums = vec![1, 2, 0, 2, 55, 66, 1, 2];
+
+    let node = Node::from_u8(&mut nums.into_iter());
+
+    assert_eq!(
+        node,
+        Ok(Node {
+            children: vec![Node {
+                children: vec![],
+                metadata: vec![55, 66]
+            }],
+            metadata: vec![1, 2]
+        })
+    );
+}
+
+#[test]
+fn parse_nested_children() {
+    let nums = vec![2, 0, 1, 0, 0, 0, 0, 0];
+    let node = Node::from_u8(&mut nums.into_iter());
+
+    assert_eq![
+        node,
+        Ok(Node {
+            children: vec![
+                Node {
+                    children: vec![Node {
+                        children: vec![],
+                        metadata: vec![]
+                    }],
+                    metadata: vec![]
+                },
+                Node {
+                    children: vec![],
+                    metadata: vec![]
+                }
+            ],
+            metadata: vec![]
+        })
+    ]
+}
+
+#[test]
+fn part_one_example() {
+    let nums = vec![2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2];
+
+    let root = Node::from_u8(&mut nums.into_iter()).expect("Should parse example nodes");
+
+    let sum = root.sum_metadata();
+
+    assert_eq!(sum, 138);
 }
