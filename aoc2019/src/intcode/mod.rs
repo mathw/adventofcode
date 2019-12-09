59466712fd8593f7abdb2dc7fcd846f54bf6d51e
Matthew Walton <matthew.walton@esendex.com>
2019-12-09 14:19:29 +0000

    2019 Make Intcode computer generic over the value type
    Some serious constraints on it as we have to use these values to index into Vec, so it has to be convertible to usize, and it has to be able to participate in various mathematical operations. Should work with any signed integer primitive, and anything else implementing the required traits in a non-surprising manner.
    
    No other code has been updated to use it yet, so this commit renders the solution unable to compile. Existing days to be modified to request Program<i32> which will match their previous behaviour.

--- aoc2019/src/intcode/mod.rs
+++ aoc2019/src/intcode/mod.rs
@@ -1,47 +1,64 @@
-use std::num::ParseIntError;
+use std::convert::TryFrom;
+use std::fmt::Display;
+use std::ops::Add;
 use std::ops::Index;
 use std::ops::IndexMut;
+use std::ops::Mul;
+use std::ops::Rem;
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Program {
-    locations: Vec<i32>,
+pub struct Program<N> {
+    locations: Vec<N>,
 }
 
-impl FromStr for Program {
-    type Err = ParseIntError;
+impl<N> FromStr for Program<N>
+where
+    N: FromStr,
+{
+    type Err = <N as FromStr>::Err;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         s.split(',')
-            .map(|n| i32::from_str(n.trim()))
-            .collect::<Result<Vec<i32>, Self::Err>>()
+            .map(|n| N::from_str(n.trim()))
+            .collect::<Result<Vec<N>, Self::Err>>()
             .map(|v| Program { locations: v })
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum State {
+pub enum State<N> {
     NeedsInput,
-    ProvidedOutput(i32),
+    ProvidedOutput(N),
     Completed,
 }
 
-pub struct RunState {
-    pub state: State,
-    runner: ProgramRunner,
+pub struct RunState<N> {
+    pub state: State<N>,
+    runner: ProgramRunner<N>,
 }
 
-impl RunState {
-    pub fn resume(self) -> RunState {
+impl<N> RunState<N>
+where
+    N: Copy
+        + PartialOrd
+        + From<i32>
+        + Display
+        + Rem<Output = N>
+        + Add<Output = N>
+        + Mul<Output = N>,
+    usize: TryFrom<N>,
+{
+    pub fn resume(self) -> RunState<N> {
         RunState::next(self.runner)
     }
 
-    pub fn resume_with_input(self, input: i32) -> RunState {
+    pub fn resume_with_input(self, input: N) -> RunState<N> {
         let mut runner = self.runner;
         runner.provide_input(input);
         RunState::next(runner)
     }
 
-    fn next(mut runner: ProgramRunner) -> RunState {
+    fn next(mut runner: ProgramRunner<N>) -> RunState<N> {
         loop {
             match runner.run_until_state_change() {
                 ProgramState::Completed => {
@@ -69,19 +86,32 @@ impl RunState {
     }
 }
 
-impl Program {
-    pub fn run(&mut self, inputs: &Vec<i32>) -> Vec<i32> {
+impl<N> Program<N>
+where
+    N: Copy
+        + PartialOrd
+        + From<i32>
+        + Display
+        + Rem<Output = N>
+        + Add<Output = N>
+        + Mul<Output = N>,
+    usize: TryFrom<N>,
+{
+    pub fn run(&mut self, inputs: &Vec<N>) -> Vec<N> {
         let (locations, outputs) = self.run_core(inputs);
         self.locations = locations;
 
         outputs
     }
 
-    pub fn run_pure(&self, inputs: &Vec<i32>) -> Vec<i32> {
+    pub fn run_pure(&self, inputs: &Vec<N>) -> Vec<N> {
         self.run_core(inputs).1
     }
 
-    pub fn run_until_needs_interaction(&self) -> RunState {
+    pub fn run_until_needs_interaction(&self) -> RunState<N>
+    where
+        N: Copy,
+    {
         let runner = ProgramRunner::new(self.locations.clone());
         let runstate = RunState {
             state: State::Completed,
@@ -90,8 +120,11 @@ impl Program {
         runstate.resume()
     }
 
-    fn run_core(&self, inputs: &Vec<i32>) -> (Vec<i32>, Vec<i32>) {
-        let mut inputs = inputs.iter().rev().cloned().collect::<Vec<i32>>();
+    fn run_core(&self, inputs: &Vec<N>) -> (Vec<N>, Vec<N>)
+    where
+        N: Copy,
+    {
+        let mut inputs = inputs.iter().rev().cloned().collect::<Vec<N>>();
         let mut outputs = Vec::new();
 
         let mut runstate = self.run_until_needs_interaction();
@@ -115,23 +148,27 @@ impl Program {
 }
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
-enum ProgramState {
+enum ProgramState<N> {
     NotStarted,
     Running,
     NeedsInput,
-    ProvidedOutput(i32),
+    ProvidedOutput(N),
     Completed,
 }
 
-struct ProgramRunner {
-    locations: Vec<i32>,
+struct ProgramRunner<N> {
+    locations: Vec<N>,
     program_counter: usize,
-    inputs: Vec<i32>,
-    state: ProgramState,
+    inputs: Vec<N>,
+    state: ProgramState<N>,
 }
 
-impl ProgramRunner {
-    fn new(locations: Vec<i32>) -> ProgramRunner {
+impl<N> ProgramRunner<N>
+where
+    N: From<i32> + Rem<Output = N> + Add<Output = N> + Mul<Output = N> + PartialOrd + Display,
+    usize: TryFrom<N>,
+{
+    fn new(locations: Vec<N>) -> ProgramRunner<N> {
         ProgramRunner {
             locations,
             program_counter: 0,
@@ -140,21 +177,28 @@ impl ProgramRunner {
         }
     }
 
-    fn run_until_state_change(&mut self) -> ProgramState {
+    fn run_until_state_change(&mut self) -> ProgramState<N> {
         while self.opcode() == ProgramState::Running {}
         self.state
     }
 
-    fn current(&self) -> i32 {
+    fn current(&self) -> N {
         self.locations[self.program_counter]
     }
 
+    fn to_usize_or_panic(n: N) -> usize {
+        match usize::try_from(n) {
+            Ok(u) => u,
+            Err(_) => panic!("This N must be convertable to usize"),
+        }
+    }
+
     fn current_opcode(&self) -> usize {
-        (self.current() % 100) as usize
+        Self::to_usize_or_panic(self.current() % 100i32.into())
     }
 
     fn binary_parameter_modes(&self) -> (Mode, Mode) {
-        if self.current() > 100 {
+        if self.current() > 100i32.into() {
             str_to_binary_modes(&self.current().to_string())
         } else {
             (Mode::Position, Mode::Position)
@@ -162,14 +206,14 @@ impl ProgramRunner {
     }
 
     fn unary_parameter_mode(&self) -> Mode {
-        if self.current() > 100 {
+        if self.current() > 100i32.into() {
             str_to_unary_mode(&self.current().to_string())
         } else {
             Mode::Position
         }
     }
 
-    fn opcode(&mut self) -> ProgramState {
+    fn opcode(&mut self) -> ProgramState<N> {
         self.state = match self.current_opcode() {
             99 => ProgramState::Completed,
             1 => self.binary_operation(|a, b| a + b),
@@ -186,24 +230,24 @@ impl ProgramRunner {
         self.state
     }
 
-    fn argument_value(&self, offset: usize, mode: Mode) -> i32 {
+    fn argument_value(&self, offset: usize, mode: Mode) -> N {
         let location_value = self.at_offset(offset);
         match mode {
             Mode::Immediate => location_value,
-            Mode::Position => self.locations[location_value as usize],
+            Mode::Position => self.locations[location_value.into()],
         }
     }
 
-    fn at_offset(&self, offset: usize) -> i32 {
+    fn at_offset(&self, offset: usize) -> N {
         self.locations[self.program_counter + offset]
     }
 
-    fn binary_operation<O>(&mut self, operation: O) -> ProgramState
+    fn binary_operation<O>(&mut self, operation: O) -> ProgramState<N>
     where
-        O: Fn(i32, i32) -> i32,
+        O: Fn(N, N) -> N,
     {
         let (mode1, mode2) = self.binary_parameter_modes();
-        let result_position = self.at_offset(3) as usize;
+        let result_position = self.at_offset(3).into();
         let first_argument = self.argument_value(1, mode1);
         let second_argument = self.argument_value(2, mode2);
 
@@ -214,15 +258,15 @@ impl ProgramRunner {
         ProgramState::Running
     }
 
-    fn provide_input(&mut self, input: i32) {
+    fn provide_input(&mut self, input: N) {
         self.inputs.push(input)
     }
 
-    fn input(&mut self) -> ProgramState {
+    fn input(&mut self) -> ProgramState<N> {
         if self.inputs.is_empty() {
             ProgramState::NeedsInput
         } else {
-            let first_argument_position = self.at_offset(1) as usize;
+            let first_argument_position = self.at_offset(1).into();
             let input = self.inputs.pop().expect("Cannot run input: no more inputs");
             self.locations[first_argument_position] = input;
             self.advance(2);
@@ -230,23 +274,23 @@ impl ProgramRunner {
         }
     }
 
-    fn output(&mut self) -> ProgramState {
+    fn output(&mut self) -> ProgramState<N> {
         let first_argument = self.argument_value(1, self.unary_parameter_mode());
         self.advance(2);
         ProgramState::ProvidedOutput(first_argument)
     }
 
-    fn jump_if(&mut self, want_true: bool) -> ProgramState {
+    fn jump_if(&mut self, want_true: bool) -> ProgramState<N> {
         let (mode1, mode2) = self.binary_parameter_modes();
         let first = self.argument_value(1, mode1);
-        let second = self.argument_value(2, mode2) as usize;
+        let second = Self::to_usize_or_panic(self.argument_value(2, mode2));
 
         if want_true {
-            if first != 0 {
+            if first != 0i32.into() {
                 self.jump(second);
                 return ProgramState::Running;
             }
-        } else if first == 0 {
+        } else if first == 0i32.into() {
             self.jump(second);
             return ProgramState::Running;
         }
@@ -263,23 +307,29 @@ impl ProgramRunner {
         self.program_counter = target
     }
 
-    fn comparative<F>(&mut self, compare: F) -> ProgramState
+    fn comparative<F>(&mut self, compare: F) -> ProgramState<N>
     where
-        F: Fn(i32, i32) -> bool,
+        F: Fn(N, N) -> bool,
     {
-        self.binary_operation(|a, b| if compare(a, b) { 1 } else { 0 })
+        self.binary_operation(|a, b| {
+            if compare(a, b) {
+                1i32.into()
+            } else {
+                0i32.into()
+            }
+        })
     }
 }
 
-impl Index<usize> for Program {
-    type Output = i32;
+impl<N> Index<usize> for Program<N> {
+    type Output = N;
 
     fn index(&self, i: usize) -> &Self::Output {
         self.locations.index(i)
     }
 }
 
-impl IndexMut<usize> for Program {
+impl<N> IndexMut<usize> for Program<N> {
     fn index_mut(&mut self, i: usize) -> &mut <Self as Index<usize>>::Output {
         self.locations.index_mut(i)
     }
@@ -332,7 +382,7 @@ fn test_str_to_binary_modes() {
 #[test]
 fn test_add_immediate() {
     // add 2 + 3 and store in 0
-    let mut program = Program::from_str("1101,2,3,0,99").unwrap();
+    let mut program = Program::<i32>::from_str("1101,2,3,0,99").unwrap();
     program.run(&vec![]);
     assert_eq!(program[0], 5);
 }
@@ -340,7 +390,7 @@ fn test_add_immediate() {
 #[test]
 fn test_add_position() {
     // add positions 5 and 6 and store in 0
-    let mut program = Program::from_str("1,5,6,0,99,1,3").unwrap();
+    let mut program = Program::<i32>::from_str("1,5,6,0,99,1,3").unwrap();
     program.run(&vec![]);
     assert_eq!(program[0], 4);
 }
@@ -348,42 +398,42 @@ fn test_add_position() {
 #[test]
 fn test_from_str_happy() {
     let input = "2,4,5,219 ,00,2920";
-    let program = Program::from_str(input).expect("This should not fail");
+    let program = Program::<i32>::from_str(input).expect("This should not fail");
     assert_eq!(program.locations, vec![2, 4, 5, 219, 0, 2920]);
 }
 
 #[test]
 fn test_from_str_unhappy() {
     let input = "2,4,potato,219,00,2920";
-    let program = Program::from_str(input);
+    let program = Program::<i32>::from_str(input);
     assert!(program.is_err());
     assert_eq!(program.err(), usize::from_str("potato").err());
 }
 
 #[test]
 fn test_input() {
-    let mut program = Program::from_str("3,3,99,5,22").unwrap();
+    let mut program = Program::<i32>::from_str("3,3,99,5,22").unwrap();
     program.run(&vec![9, 8]);
     assert_eq!(program[3], 9);
 }
 
 #[test]
 fn test_output() {
-    let program = Program::from_str("4,3,99,5").unwrap();
+    let program = Program::<i32>::from_str("4,3,99,5").unwrap();
     let outputs = program.run_pure(&vec![]);
     assert_eq!(outputs, vec![5]);
 }
 
 #[test]
 fn test_run_sample() {
-    let mut program = Program::from_str("1,9,10,3,2,3,11,0,99,30,40,50").unwrap();
+    let mut program = Program::<i64>::from_str("1,9,10,3,2,3,11,0,99,30,40,50").unwrap();
     program.run(&mut vec![]);
     assert_eq!(program[0], 3500);
 }
 
 #[test]
 fn test_output_immediate() {
-    let program = Program::from_str("104, 2, 99, 5, 22").unwrap();
+    let program = Program::<i128>::from_str("104, 2, 99, 5, 22").unwrap();
     let outputs = program.run_pure(&vec![]);
     assert_eq!(outputs, vec![2]);
 }
@@ -391,7 +441,8 @@ fn test_output_immediate() {
 #[test]
 fn test_day5_part2_sample1() {
     // checks if input == 8, outputs 1 if so, 0 if not
-    let program = Program::from_str("3,9,8,9,10,9,4,9,99,-1,8").expect("Program should parse");
+    let program =
+        Program::<i32>::from_str("3,9,8,9,10,9,4,9,99,-1,8").expect("Program should parse");
 
     // check == 8
     let outputs = program.run_pure(&mut vec![8]);
@@ -405,7 +456,7 @@ fn test_day5_part2_sample1() {
 #[test]
 fn test_day5_part2_sample3() {
     // checks if input == 8, outputs 1 if so, 0 if not
-    let program = Program::from_str("3,3,1108,-1,8,3,4,3,99").expect("Program should parse");
+    let program = Program::<i32>::from_str("3,3,1108,-1,8,3,4,3,99").expect("Program should parse");
 
     // check == 8
     let outputs = program.run_pure(&mut vec![8]);
@@ -421,7 +472,7 @@ fn test_day5_part2_big_sample() {
     // takes one input
     // outputs 999 if input < 8, 1000 if input == 8, 1001 if input > 8
     // uses jump instructions to do it
-    let program = Program::from_str(
+    let program = Program::<i32>::from_str(
         "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
